cdf1b85bfe3879fa843796e0a131d1aa0852eb42
JeffLawrence1 <jefflaw13@hotmail.com>
2019-04-26 16:36:03 -0700

    one done

--- code-challenges/301/string-methods/challenges-05.test.js
+++ code-challenges/301/string-methods/challenges-05.test.js
@@ -12,6 +12,11 @@ For example, if the input is 'Welcome', the output will be:
 const howMuchPencil = (str) => {
   let result = [];
   // Solution code here...
+
+  for(let i = 0; i < str.length; i++){
+    result.push(str.slice(i));
+  }
+  result.push('');
   return result;
 };
 
