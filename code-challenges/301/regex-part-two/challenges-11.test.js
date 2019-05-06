22fd331fdf2b3b598b6600bc638e25119a765654
JeffLawrence1 <jefflaw13@hotmail.com>
2019-05-06 16:52:07 -0700

    one done

--- code-challenges/301/regex-part-two/challenges-11.test.js
+++ code-challenges/301/regex-part-two/challenges-11.test.js
@@ -8,6 +8,8 @@ If the PIN is four numerical digits long, return true. Otherwise, return false.
 
 const validatePin = (pin) => {
   // Solution code here...
+  let regex = /^(\d{4})$/g;
+  return regex.test(pin);
 };
 
 /* ------------------------------------------------------------------------------------------------
