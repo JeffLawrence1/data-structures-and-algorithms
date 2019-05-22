78f6803be497b5a17a96b8ac90ac537823726d80
JeffLawrence1 <jefflaw13@hotmail.com>
2019-05-22 14:19:12 -0700

    algo translated from whiteboard readme added

---
+++ code-challenges/401/arrayBinarySearch/Readme.md
@@ -0,0 +1,12 @@
+# Reverse an Array
+<!-- Short summary or background information -->
+Reversing an array
+## Challenge
+<!-- Description of the challenge -->
+Challenge is to reverse an array without using any js built ins
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+Whiteboarded then coded
+## Solution
+<!-- Embedded whiteboard image -->
+* [whiteboard image](../assets/array-binary-search.jpg)
--- code-challenges/401/arrayBinarySearch/array-binary-search.js
+++ code-challenges/401/arrayBinarySearch/array-binary-search.js
@@ -1,10 +1,9 @@
 'use strict';
 
-function BinarySearch(arr, val){
+function binarySearch(arr, val){
   let start =0;
   let end = arr.length - 1;
   let mid = 0;
-  let answer = -1;
 
   while(start <= end){
     mid = Math.floor((start + end) / 2);
@@ -13,9 +12,11 @@ function BinarySearch(arr, val){
     }else if(arr[mid] > val){
       end = mid - 1;
     }else{
-      answer = mid;
+      return mid;
     }
   }
-  return answer;
+  return -1;
 }
 
+console.log(binarySearch([1, 3, 5, 6], 3));
+console.log(binarySearch([1, 3, 5, 6], 2));
