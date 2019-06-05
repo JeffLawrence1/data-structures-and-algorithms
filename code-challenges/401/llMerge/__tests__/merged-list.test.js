77c1786d7feb430f1288c6acdeafc3e7a039534d
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-05 13:23:21 -0700

    update mergeL function

--- code-challenges/401/llMerge/__tests__/merged-list.test.js
+++ code-challenges/401/llMerge/__tests__/merged-list.test.js
@@ -1,25 +1,26 @@
 'use strict';
 
-const linkedList = require('../../linkList/linked-list.js');
+const linkedList = require('../../linkedList/linked-list.js');
 const mergeList = require('../ll-merge.js');
 
 describe('mergedList module', () => {
 
-  let newList1;
-  let newList2;
+  let list1;
+  let list2;
 
   beforeEach(() => {
-    newList1 = new linkedList();
-    newList2 = new linkedList();
+    list1 = new linkedList();
+    list2 = new linkedList();
   });
 
   it('It can merge two linked lists', () => {
     let node3 = {val: 3, next: null};
     let node2 = {val: 2, next: node3};
     let node1 = {val: 1, next: node2};
-    newList.head = node1;
-    newList.length = 3;
-    // console.error(newList.kthFromEnd(0));
-    expect(newList.kthFromEnd(1)).toEqual(2);
+    list1.head = node1;
+    list1.length = 3;
+    list2.head = node1;
+    list2.length = 3;
+    expect(mergeList(list1, list2)).toEqual(list1.head);
   });
 });
