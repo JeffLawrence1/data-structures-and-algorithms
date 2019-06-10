9ec38ccc44301af200b34f1712097ab34149ddf7
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-10 13:06:48 -0700

    added stack class

--- code-challenges/401/queueWithStacks/queue_with_stacks.js
+++ code-challenges/401/queueWithStacks/queue_with_stacks.js
@@ -7,6 +7,30 @@ class Node {
   }
 }
 
+class Stack {
+  constructor(){
+    this.top = null;
+  }
+
+  push(val){
+    if (!val) return false;
+    let node = new Node(val);
+    node.next = this.top;
+    this.top = node;
+  }
+
+  pop(){
+    this.top = this.top.next;
+  }
+
+  peek() {
+    if(!this.top){
+      return null;
+    }else{
+      return this.top.val;
+    }
+  }
+}
 
 
 
