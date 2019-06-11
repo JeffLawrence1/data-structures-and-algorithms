206b45b8129e2efa812819c5790ad5f21f135039
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-11 13:08:09 -0700

    created animal and shelter

--- code-challenges/401/fifoAnimalShelter/fifo-animal-shelter.js
+++ code-challenges/401/fifoAnimalShelter/fifo-animal-shelter.js
@@ -1,1 +1,19 @@
 'use strict';
+
+class Animal{
+  constructor(name){
+    if(name !== 'cat' || name !== 'dog'){
+      return 'must be cat or dog';
+    }
+    this.name = name;
+  }
+}
+class AnimalShelter{
+  constructor(){
+    this.storage = [];
+  }
+
+
+}
+
+module.exports = AnimalShelter;
