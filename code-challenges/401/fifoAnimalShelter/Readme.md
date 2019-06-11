3ef54f4c63a326346da7a9b38dd55688e503ca69
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-11 12:52:05 -0700

    init commit

---
+++ code-challenges/401/fifoAnimalShelter/Readme.md
@@ -0,0 +1,15 @@
+# Fifo Animal Shelter
+<!-- Short summary or background information -->
+Animal Shelter class that uses a queue
+
+## Challenge
+<!-- Description of the challenge -->
+implement an enqueue function that adds a cat or dog and a dequeue function that removes the first cat or dog in line whichever was called or returns null if it wasn't a cat or dog
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+I paid attention to the inclass demo, did some google research, a little planning and used the whiteboard, then got to work
+
+## API
+<!-- Description of each method publicly available to your Linked List -->
+AnimalShelter class <--- creates new animal and has enqueue and dequeue functions
---
+++ code-challenges/401/fifoAnimalShelter/__tests__/fifo-animal-shelter.test.js
@@ -0,0 +1,126 @@
+'use strict';
+
+const animalShelter = require('../fifo-animal-shelter.js');
+
+// describe('Singly Linked List Module', () => {
+
+//   let newList;
+
+//   beforeEach(() => {
+//     newList = new linkedList();
+//   });
+
+//   it('Can successfully instantiate an empty linked list', () => {
+
+//     expect(newList).toBeDefined();
+//   });
+
+//   it('Can properly insert into the linked list', () => {
+//     newList.insert(1);
+
+//     expect(newList.insert(1)).toEqual(1);
+//   });
+
+//   it('The head property will properly point to the first node in the linked list', () => {
+//     newList.insert(2);
+//     expect(newList.head.val).toEqual(2);
+//   });
+
+//   it('Can properly insert multiple nodes into the linked list', () => {
+//     newList.insert(1);
+//     newList.insert(2);
+//     newList.insert(3);
+//     expect(newList.head.next.next.val).toEqual(1);
+//   });
+
+//   it('Will return true when finding a value within the linked list that exists', () => {
+//     newList.insert(11);
+//     newList.insert(22);
+//     newList.insert(33);
+//     expect(newList.includes(22)).toEqual(true);
+//   });
+
+//   it('Will NOT return true when finding a value within the linked list that exists', () => {
+//     newList.insert(11);
+//     newList.insert(22);
+//     newList.insert(33);
+//     expect(newList.includes(12)).toEqual(false);
+//   });
+
+//   describe('print', () => {
+//     it('should console.log once for every item in List', () => {
+//       let mockConsoleLog = jest.spyOn(console, 'log');
+//       mockConsoleLog.mockImplementation(() => true);
+
+//       //act
+//       newList.print();
+
+//       //assert
+//       expect(mockConsoleLog).not.toBeCalled();
+//       // expect(mockConsoleLog).toBeCalled();
+//     });
+//   });
+
+//   describe('append', () => {
+//     it('Can successfully add a node to the end of the linked list', () => {
+//       newList.insert(22);
+//       newList.append(11);
+//       expect(newList.head.next.val).toEqual(11);
+
+//     });
+
+//     it('Can successfully add a node to the beginning of the linked list', () => {
+//       newList.append(11);
+//       expect(newList.head.val).toEqual(11);
+
+//     });
+
+//     it('Can successfully add multiple nodes to the end of a linked list', () => {
+//       newList.insert(22);
+//       newList.append(11);
+//       newList.append(1);
+//       expect(newList.head.next.val).toEqual(11);
+//       expect(newList.head.next.next.val).toEqual(1);
+
+//     });
+//   });
+
+//   describe('insertBefore', () => {
+//     it('Can successfully insert a node before a node located in the middle of a linked list', () => {
+//       newList.insert(11);
+//       newList.insert(22);
+//       newList.insert(33);
+//       newList.insertBefore(22, 2);
+//       expect(newList.head.next.val).toEqual(2);
+
+//     });
+
+//     it('Can successfully insert a node before the first node of a linked list', () => {
+//       newList.insert(11);
+//       newList.insert(22);
+//       newList.insert(33);
+//       newList.insertBefore(33, 2);
+//       expect(newList.head.val).toEqual(2);
+//     });
+//   });
+
+//   describe('insertAfter', () => {
+//     it('Can successfully insert after a node in the middle of the linked list', () => {
+//       newList.insert(11);
+//       newList.insert(22);
+//       newList.insert(33);
+//       newList.insertAfter(22, 2);
+//       expect(newList.head.next.next.val).toEqual(2);
+
+//     });
+
+//     it('Can successfully insert a node after the last node of the linked list', () => {
+//       newList.insert(11);
+//       newList.insert(22);
+//       newList.insert(33);
+//       newList.insertAfter(11, 2);
+//       expect(newList.head.next.next.next.val).toEqual(2);
+//     });
+//   });
+});
+
