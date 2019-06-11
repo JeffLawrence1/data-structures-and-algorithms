60c2539e7ef101ad96c12386e5287e3c2f2c96b6
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-11 13:08:59 -0700

    dequeu

--- code-challenges/401/fifoAnimalShelter/fifo-animal-shelter.js
+++ code-challenges/401/fifoAnimalShelter/fifo-animal-shelter.js
@@ -12,7 +12,27 @@ class AnimalShelter{
   constructor(){
     this.storage = [];
   }
+  enqueue(name){
+    let animal = new Animal(name);
+    this.storage.push(animal);
+  }
 
+  dequeue(pref){
+    let temp;
+    let result;
+    if(pref === undefined){
+      return this.storage.shift();
+    }
+    for(let i = 0; i < this.storage.length; i++){
+      temp = this.storage.shift();
+      if(temp.name === pref && temp.name !== result){
+        result = temp;
+      }else{
+        this.storage.push(temp);
+      }
+    }
+    return result;
+  }
 
 }
 
