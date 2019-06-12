a1101c557f97904a60c6c6f55d0e2a77c999311a
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-12 12:47:55 -0700

    function work

---
+++ code-challenges/401/multiBracketValidation/multi-bracket-validation.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function multiBracketValidation(input){
+
+  let arr = input.split('');
+  let compareArr = [];
+  let refArr = ['(',')','[',']','{','}'];
+
+  for(let i = 0; i < arr.length; i++){
+    let shift = arr.shift();
+    if(refArr.instanceof(shift)){
+      if(shift === '(' || shift === '[' || shift === '{'){
+        compareArr.push(shift);
+      }else{
+        if(shift === ')' && compareArr.pop() !== '('){
+          return false;
+        }else if(shift === ']' && compareArr.pop() !== '['){
+          return false;
+        }else if(shift === '}' && compareArr.pop() !== '{'){
+          return false;
+        }
+      }
+    }
+  }
+}
+
+module.exports = multiBracketValidation;
