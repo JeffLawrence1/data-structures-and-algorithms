981df8ffd64b7fd20e75f424b382565e53fa128a
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-12 13:04:14 -0700

    test work

--- code-challenges/401/multiBracketValidation/__tests__/multi-bracket-validation.test.js
+++ code-challenges/401/multiBracketValidation/__tests__/multi-bracket-validation.test.js
@@ -2,71 +2,72 @@
 
 const bracketValidation = require('../multi-bracket-validation.js');
 
-// describe('Animal Shelter', () => {
+describe('Multi Bracket Validation', () => {
 
-//   let newShelter;
-//   console.log(newShelter);
+  //   let newShelter;
+  //   console.log(newShelter);
 
-//   beforeEach(() => {
-//     newShelter = new AnimalShelter();
-//   });
+  //   beforeEach(() => {
+  //     newShelter = new AnimalShelter();
+  //   });
 
-//   describe('constructor', () => {
+  //   describe('constructor', () => {
+  let match = '(jf[df]sdf{ee})';
 
-//     it('Can successfully instantiate an instance of AnimalShelter', () => {
-//       expect(newShelter).toBeDefined();
-//       expect(newShelter).toBeInstanceOf(AnimalShelter);
-//     });
+  it('Can successfully return true if brackets match', () => {
 
-//     it('Should create an empty storage', () => {
-//       expect(newShelter.storage).toBeDefined();
-//       expect(newShelter.storage).toBeInstanceOf(Array);
-//     });
-//   });
+    expect(bracketValidation(match)).toBe(true);
+  });
 
-//   describe('enqueue', () => {
+  //     it('Should create an empty storage', () => {
+  //       expect(newShelter.storage).toBeDefined();
+  //       expect(newShelter.storage).toBeInstanceOf(Array);
+  //     });
+  //   });
 
-//     it('Can properly enqueue into the shelter', () => {
-//       newShelter.enqueue('cat');
-//       expect(newShelter.storage[0].name).toEqual('cat');
-//       expect(newShelter.storage.length).toEqual(1);
-//     });
+  //   describe('enqueue', () => {
 
-//     it('Should not add an animal if no value is provided', () => {
-//       expect(newShelter.storage).toBeDefined();
-//       expect(newShelter.storage).toBeInstanceOf(Array);
-//       expect(newShelter.storage.length).toEqual(0);
-//     });
+  //     it('Can properly enqueue into the shelter', () => {
+  //       newShelter.enqueue('cat');
+  //       expect(newShelter.storage[0].name).toEqual('cat');
+  //       expect(newShelter.storage.length).toEqual(1);
+  //     });
 
-//     it('Should return must be cat or dog if the name input is something else', () => {
-//       expect(newShelter.enqueue('rabbit')).toEqual('must be cat or dog');
-//     });
-//   });
+  //     it('Should not add an animal if no value is provided', () => {
+  //       expect(newShelter.storage).toBeDefined();
+  //       expect(newShelter.storage).toBeInstanceOf(Array);
+  //       expect(newShelter.storage.length).toEqual(0);
+  //     });
 
-//   describe('dequeue', () => {
+  //     it('Should return must be cat or dog if the name input is something else', () => {
+  //       expect(newShelter.enqueue('rabbit')).toEqual('must be cat or dog');
+  //     });
+  //   });
 
-//     it('Should remove the oldest specified animal from the queue', () => {
-//       newShelter.enqueue('cat');
-//       newShelter.enqueue('dog');
-//       newShelter.enqueue('cat');
-//       let popped = newShelter.dequeue('dog');
-//       expect(popped).toEqual({name:'dog'});
-//       expect(newShelter).toBeDefined();
-//       expect(newShelter).toBeInstanceOf(AnimalShelter);
-//       expect(newShelter.storage.length).toEqual(2);
-//     });
+  //   describe('dequeue', () => {
 
-//     it('Should not remove any other animal', () => {
-//       newShelter.enqueue('cat');
-//       newShelter.enqueue('dog');
-//       newShelter.enqueue('cat');
-//       newShelter.dequeue('dog');
-//       expect(newShelter.storage[0].name).toEqual('cat');
-//       expect(newShelter.storage[1].name).toEqual('cat');
-//     });
+  //     it('Should remove the oldest specified animal from the queue', () => {
+  //       newShelter.enqueue('cat');
+  //       newShelter.enqueue('dog');
+  //       newShelter.enqueue('cat');
+  //       let popped = newShelter.dequeue('dog');
+  //       expect(popped).toEqual({name:'dog'});
+  //       expect(newShelter).toBeDefined();
+  //       expect(newShelter).toBeInstanceOf(AnimalShelter);
+  //       expect(newShelter.storage.length).toEqual(2);
+  //     });
+
+  //     it('Should not remove any other animal', () => {
+  //       newShelter.enqueue('cat');
+  //       newShelter.enqueue('dog');
+  //       newShelter.enqueue('cat');
+  //       newShelter.dequeue('dog');
+  //       expect(newShelter.storage[0].name).toEqual('cat');
+  //       expect(newShelter.storage[1].name).toEqual('cat');
+  //     });
 
 //     it('Should return undefined if no parameter given', () => {
 //       expect(newShelter.dequeue()).toBeUndefined();
 //     });
 //   });
-// });
+});
