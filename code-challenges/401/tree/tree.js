092cd61e368e81dd434c8a2522af18a2bf30b9cf
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-14 16:39:27 -0700

    walk function

--- code-challenges/401/tree/tree.js
+++ code-challenges/401/tree/tree.js
@@ -13,26 +13,63 @@ class BinarySearchTree {
     this.root = node;
   }
 
+  preOrder(){
+
+  }
+
+  inOrder(){
+    let results = [];
+
+    let _walk = node => {
+      // L Root R
+
+      //Left
+      if(node.left){
+        _walk(node.left);
+      }
+
+      //Root
+      results.push(node.value);
+
+      //Right
+      if(node.right){
+        _walk(node.right);
+      }
+    };
+    _walk(this.root);
+    return results;
+  }
+
   add(node){
     if(!this.root){
       this.root = node;
       return node;
     }
-
     let currentNode = this.root;
     while(currentNode){
-      
-    }
-
-    if(node.value < currentNode.value){
-      currentNode.left = node;
-    }else if(node.value > currentNode.value){
-      currentNode.right = node;
-    }else{
-      //even node values
-      throw new Error('value already exists in binary search tree');
+      if(node.value < currentNode.value){
+        //go left
+        if(!currentNode.left){
+          currentNode.left = node;
+          break;
+        }else{
+          currentNode = currentNode.left;
+        }
+      }else if(node.value > currentNode.value){
+        //go right
+        if(!currentNode.right){
+          currentNode.right = node;
+          break;
+        }else{
+          currentNode = currentNode.right;
+        }
+      }else{
+        //duplicate node values
+        throw new Error('value already exists in binary search tree');
+      }
     }
   }
+
 }
 
-module.exports = BinarySearchTree;
+module.exports = Node, BinarySearchTree;
