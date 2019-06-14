ad27e6980c23e92c13a486cd626a222a526348f9
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-14 16:21:19 -0700

    add work

--- code-challenges/401/tree/tree.js
+++ code-challenges/401/tree/tree.js
@@ -12,6 +12,27 @@ class BinarySearchTree {
   constructor(node){
     this.root = node;
   }
+
+  add(node){
+    if(!this.root){
+      this.root = node;
+      return node;
+    }
+
+    let currentNode = this.root;
+    while(currentNode){
+      
+    }
+
+    if(node.value < currentNode.value){
+      currentNode.left = node;
+    }else if(node.value > currentNode.value){
+      currentNode.right = node;
+    }else{
+      //even node values
+      throw new Error('value already exists in binary search tree');
+    }
+  }
 }
 
 module.exports = BinarySearchTree;
