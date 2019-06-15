f76806cf40030347a02f0c79af661492c42a8c09
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-15 14:15:22 -0700

    test work

--- code-challenges/401/tree/__tests__/tree.test.js
+++ code-challenges/401/tree/__tests__/tree.test.js
@@ -10,99 +10,57 @@ const trees = require('../tree');
 //          2   22 33   48
 
 describe('Binary Tree Tests', () => {
-
+  let tree;
   beforeEach(() => {
     tree = new trees.BinaryTree();
+    //root
+    tree.root = {value: 20, left: null, right: null};
+    //root.left
+    tree.root.left = {value: 10, left: null, right: null};
+    //root.left.left
+    tree.root.left.left = {value: 2, left: null, right: null};
+    //root.left.right
+    tree.root.left.right = {value: 2, left: null, right: null};
+    //root.right
+    tree.root.right = {value: 25, left: null, right: null};
+    //root.right.right
+    tree.root.right.right = {value: 48, left: null, right: null};
+    //root.right.left
+    tree.root.right.left = {value: 33, left: null, right: null};
   });
 
-  it('Can successfully push onto a stack', () => {
-    stack.push(2);
-    expect(stack.top.val).toBe(2);
+  it('Can successfully instantiate an empty tree', () => {
+    expect(tree).toBeDefined();
+    expect(tree).toBeInstanceOf(trees.BinaryTree);
   });
 
-  it('Can successfully push multiple nodes onto a stack', () => {
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    expect(stack.top.val).toBe(3);
+  it('an successfully instantiate a tree with a single root node', () => {
+    expect(tree).toBeDefined();
+    expect(tree).toBeInstanceOf(trees.BinaryTree);
+    expect(tree.root).toBeDefined();
   });
 
-  it('Can successfully pop off the stack', () => {
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    stack.pop();
-    expect(stack.top.val).toBe(2);
+  it('Can successfully add a left child and right child to a single root node', () => {
+    expect(tree).toBeDefined();
+    expect(tree).toBeInstanceOf(trees.BinaryTree);
+    expect(tree.root).toBeDefined();
   });
 
-  it('Can successfully empty a stack after multiple pops', () => {
-    stack.push(1);
-    stack.push(2);
-    stack.pop();
-    stack.pop();
-    expect(stack.top).toBeNull();
+  it('Can successfully return a collection from a preorder traversal', () => {
+    expect(tree).toBeDefined();
+    expect(tree).toBeInstanceOf(trees.BinaryTree);
+    expect(tree.root).toBeDefined();
   });
 
-  it('Can successfully peek the next item on the stack.', () => {
-    stack.push(1);
-    stack.push(2);
-    expect(stack.peek()).toBe(2);
+  it('Can successfully return a collection from an inorder traversal', () => {
+    expect(tree).toBeDefined();
+    expect(tree).toBeInstanceOf(trees.BinaryTree);
+    expect(tree.root).toBeDefined();
   });
 
-  it('Can successfully instantiate an empty stack', () => {
-    expect(stack).toBeInstanceOf(Stack);
-    expect(stack.peek()).toBeNull();
-  });
-});
-
-describe('Queue Testing', () => {
-  let queue;
-
-  beforeEach(() => {
-    queue = new Queue();
-  });
-
-  it('Can successfully enqueue into a queue', () => {
-    queue.enqueue(1);
-    expect(queue.front.val).toBe(1);
-  });
-
-  it('Can successfully enqueue multiple values into a queue', () => {
-    queue.enqueue(1);
-    queue.enqueue(2);
-    queue.enqueue(3);
-    expect(queue.front.val).toBe(1);
-    expect(queue.front.next.val).toBe(2);
-    expect(queue.front.next.next.val).toBe(3);
-  });
-
-  it('Can successfully dequeue out of a queue the expected value', () => {
-    queue.enqueue(1);
-    queue.enqueue(2);
-    queue.dequeue();
-    expect(queue.front.val).toBe(2);
-  });
-
-  it('Can successfully peek into a queue, seeing the expected value', () => {
-    queue.enqueue(1);
-    queue.enqueue(2);
-    expect(queue.peek()).toBe(1);
-  });
-
-  it('Can successfully empty a queue after multiple dequeues', () => {
-    queue.enqueue(1);
-    queue.enqueue(2);
-    queue.enqueue(3);
-    queue.dequeue();
-    queue.dequeue();
-    queue.dequeue();
-    expect(queue).toBeInstanceOf(Queue);
-    expect(queue.front).toBeNull();
-  });
-
-  it('Can successfully instantiate an empty queue', () => {
-    expect(queue).toBeInstanceOf(Queue);
-    expect(queue.peek()).toBeNull();
+  it('Can successfully return a collection from a postorder traversal', () => {
+    expect(tree).toBeDefined();
+    expect(tree).toBeInstanceOf(trees.BinaryTree);
+    expect(tree.root).toBeDefined();
   });
 });
-
