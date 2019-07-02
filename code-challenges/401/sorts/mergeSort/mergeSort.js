16440b24945f011839e9b11d10b75f63dce631da
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-02 12:30:06 -0700

    merge

--- code-challenges/401/sorts/mergeSort/mergeSort.js
+++ code-challenges/401/sorts/mergeSort/mergeSort.js
@@ -1,31 +1,7 @@
-
-
-ALGORITHM Merge(b, c, a)
-    DECLARE i <-- 0
-    DECLARE j <-- 0
-    DECLARE k <-- 0
-
-    while i < b && j < c
-        if b[i] <= c[j]
-            a[k] <-- b[i]
-            i <-- i + 1
-        else
-            a[k] = c[j]
-            j <-- j + 1
-            
-        k <-- k + 1
-
-    if i = b.length
-       add remaining items in array c to array a
-    else
-       add remaining items in array b to array a
-       
-    return a
-
 'use strict';
 
 function mergeSort(arr){
-  let n = arr.length
+  let n = arr.length;
   if(arr.length > 1){
     let mid = n / 2;
     let b = arr.slice(0, mid);
@@ -35,6 +11,31 @@ function mergeSort(arr){
     // break down the right side
     mergeSort(c);
     // merge the left and the right side together
-    mergeSort(b, c, arr);
+    merge(b, c, arr);
   }
 }
+
+function merge(b, c, a){
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  while(i < b && j < c){
+    if(b[i] <= c[j]){
+      a[k] = b[i];
+      i = i + 1;
+    }else{
+      a[k] = b[i];
+      j = j + 1;
+    }
+    k = k + 1;
+  }
+  if(i === b.length){
+    a.concat(c);
+  }else{
+    a.concat(b);
+  }
+  return a;
+}
+
+module.exports = mergeSort;
