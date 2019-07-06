9c8204620e562c1ea123af9065ab6f2fa5a887f1
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-06 10:47:02 -0700

    added tests

--- code-challenges/401/hashtable/__tests__/hashtable.test.js
+++ code-challenges/401/hashtable/__tests__/hashtable.test.js
@@ -19,6 +19,21 @@ describe('Hash Table', () =>{
     expect(hashTable.buckets.length).toBe(expectedSize);
   });
 
+  it('should construct a Hash Table with a default size of 5', () => {
+    //arrange
+    let hashTable;
+    let expectedSize = 5;
+    //act
+    hashTable = new HashTable();
+
+    //assert
+    expect(hashTable).toBeInstanceOf(HashTable);
+    expect(hashTable.size).toBeDefined();
+    expect(hashTable.size).toBe(expectedSize);
+    expect(hashTable.buckets).toBeDefined();
+    expect(hashTable.buckets.length).toBe(expectedSize);
+  });
+
   describe('Hash key function', () => {
     it('should hash cat to 3 with 5 buckets', () => {
       //arrange
@@ -32,23 +47,34 @@ describe('Hash Table', () =>{
     });
 
     it('should hash foo to 1 with 5 buckets', () => {
+      //arrange
       let expectedHash = 1;
       let key = 'foo';
       let hashTable = new HashTable(5);
-
+      //act
       let result = hashTable.hash(key);
-
+      //assert
       expect(result).toBe(expectedHash);
     });
   });
 });
 
+describe('Singly Linked List Module', () => {
 
+  let newList;
 
+  beforeEach(() => {
+    newList = new LinkedList();
+  });
 
+  it('Can successfully instantiate an empty linked list', () => {
 
-// Hash(key){
-//   return key.split('').reduce((prev, curr) => {
-//     return prev + curr.charCodeAt(0);
-//   }, 0) * 599 % this.size;
-// }
+    expect(newList).toBeDefined();
+  });
+
+  it('Can properly insert into the linked list', () => {
+    newList.add(1);
+
+    expect(newList.head.value).toEqual(1);
+  });
+});
