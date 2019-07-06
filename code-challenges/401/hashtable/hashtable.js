ce6153a45ffbe5c763ff51110c15f9576b4a2466
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-06 10:00:10 -0700

    ll created

--- code-challenges/401/hashtable/hashtable.js
+++ code-challenges/401/hashtable/hashtable.js
@@ -1,9 +1,52 @@
 'use strict';
 
-class HashTable{
-  constructor(){
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
 
-    this.size = 
+class LinkedList {
+  constructor(){
+    this.head = null;
+  }
+
+  add(value){
+    if(!value) throw new Error('invalid value');
+
+    const node = new Node(value);
+
+    if(!this.head){
+      this.head = node;
+      return;
+    }
+
+    let current = this.head;
+    while(current.next){
+      current = current.next;
+    }
+    current.next = node;
+  }
+
+  values(){
+    let values = [];
+    let current = this.head;
+
+    while(current){
+      values.push(current.value);
+      current = current.next;
+    }
+
+    return values;
+  }
+}
+
+class HashTable{
+  constructor(size){
+
+    this.size = size || 5;
+    this.buckets = new Array(this.size);
   }
 }
 
