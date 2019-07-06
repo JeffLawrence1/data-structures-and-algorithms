2690b4071aa788fd7575196aa3da7b57d468724c
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-06 15:34:30 -0700

    testing

--- code-challenges/401/hashtable/__tests__/hashtable.test.js
+++ code-challenges/401/hashtable/__tests__/hashtable.test.js
@@ -34,28 +34,92 @@ describe('Hash Table', () =>{
     expect(hashTable.buckets.length).toBe(expectedSize);
   });
 
-  describe('Hash key function', () => {
-    it('should hash cat to 3 with 5 buckets', () => {
-      //arrange
-      let expectedHash = 3;
-      let key = 'cat';
-      let hashTable = new HashTable(5);
-      //act
-      let result = hashTable.hash(key);
-      //assert
-      expect(result).toBe(expectedHash);
-    });
+  it('Successfully returns null for a key that does not exist in the hashtable', () => {
+    //arrange
+    let key = 'cat';
+    let hashTable = new HashTable();
+    //act
+    //assert
+    expect(hashTable.get(key)).toBe(null);
+  });
 
-    it('should hash foo to 1 with 5 buckets', () => {
-      //arrange
-      let expectedHash = 1;
-      let key = 'foo';
-      let hashTable = new HashTable(5);
-      //act
-      let result = hashTable.hash(key);
-      //assert
-      expect(result).toBe(expectedHash);
-    });
+  it('Successfully handle a collision within the hashtable', () => {
+    //arrange
+    let key = 'cat';
+    let key2 = 'bbt';
+    let value = 'pizzacat';
+    let hashTable = new HashTable();
+    //act
+    hashTable.add(key, value);
+    hashTable.add(key2, value);
+    //assert
+    expect(hashTable.contains(key)).toBe(true);
+    expect(hashTable.contains(key2)).toBe(true);
+  });
+
+  it('Successfully retrieve a value from a bucket within the hashtable that has a collision', () => {
+    //arrange
+    let key = 'cat';
+    let key2 = 'bbt';
+    let value = 'pizzacat';
+    let hashTable = new HashTable();
+    //act
+    hashTable.add(key, value);
+    hashTable.add(key2, value);
+    //assert
+    expect(hashTable.get(key)).toBe(value);
+    expect(hashTable.get(key2)).toBe(value);
+  });
+});
+
+describe('Hash key function', () => {
+  it('should hash cat to 3 with 5 buckets', () => {
+    //arrange
+    let expectedHash = 3;
+    let key = 'cat';
+    let hashTable = new HashTable(5);
+    //act
+    let result = hashTable.hash(key);
+    //assert
+    expect(result).toBe(expectedHash);
+  });
+
+  it('should hash foo to 1 with 5 buckets', () => {
+    //arrange
+    let expectedHash = 1;
+    let key = 'foo';
+    let hashTable = new HashTable(5);
+    //act
+    let result = hashTable.hash(key);
+    //assert
+    expect(result).toBe(expectedHash);
+  });
+});
+
+describe('Hash add function', () => {
+  it('should contain cat be true', () => {
+    //arrange
+    // let expectedHash = 3;
+    let key = 'cat';
+    let value = 'tacocat';
+    let hashTable = new HashTable();
+    //act
+    hashTable.add(key, value);
+    //assert
+    expect(hashTable.contains('cat')).toBe(true);
+  });
+});
+
+describe('Hash get function', () => {
+  it('should get tacocat', () => {
+    //arrange
+    let key = 'cat';
+    let value = 'tacocat';
+    let hashTable = new HashTable();
+    //act
+    hashTable.add(key, value);
+    //assert
+    expect(hashTable.get('cat')).toBe('tacocat');
   });
 });
 
