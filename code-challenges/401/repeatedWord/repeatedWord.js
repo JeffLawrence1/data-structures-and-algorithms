50932e9e5121087de7396a0120616d52df0b0326
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-08 13:25:49 -0700

    works

--- code-challenges/401/repeatedWord/repeatedWord.js
+++ code-challenges/401/repeatedWord/repeatedWord.js
@@ -4,12 +4,15 @@ const HashTable = require('../hashtable/hashtable');
 
 function repeatedWord(string){
   let hashTable = new HashTable.HashTable(10);
-  let tempArr = string.split('');
+  let tempArr = string.split(' ');
   let answer;
   let word;
   for(let i = 0; i < tempArr.length; i++){
-    word = tempArr[i].replace(/\w+/g).toLowerCase();
-    answer = hashTable.add(word, word);
+    // console.log(tempArr[i]);
+    word = tempArr[i].replace(/([^\s\w])/g, '').toLowerCase();
+    // console.log(word);
+    answer = hashTable.add(word, 1);
+    // console.log(answer);
     if(answer === 'key already being used'){
       return word;
     }
@@ -17,4 +20,4 @@ function repeatedWord(string){
   return 'no duplicates found';
 }
 
-console.log(repeatedWord('this is a very long sentence this to test'));
+console.log(repeatedWord('this, is a ve,ry long sente,nce this! to test'));
