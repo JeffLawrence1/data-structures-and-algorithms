f32206351715264a3412099b22a5d1b0f126d714
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-09 13:03:37 -0700

    function

--- code-challenges/401/treeIntersection/tree-intersection.js
+++ code-challenges/401/treeIntersection/tree-intersection.js
@@ -1,2 +1,21 @@
 'use strict';
 
+
+const BinaryTree = require('../tree/tree');
+
+function treeIntersection(tree1, tree2){
+  let treeOne = tree1.preOrder();
+  let treeTwo = tree2.preOrder();
+  let answerArr = [];
+  for(let i = 0; i < treeOne.length; i++){
+    for(let j = 0; j < treeTwo.length; i++){
+      if(treeOne[i] === treeTwo[j]){
+        answerArr.push(treeOne[i]);
+      }
+    }
+  }
+  return answerArr;
+}
+
+module.exports = treeIntersection;
+
