00ceece24bb2427b8b16ba95e1cabd2e76bc2a3f
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-14 11:13:46 -0700

    update graph names

--- code-challenges/401/graph/graph.js
+++ code-challenges/401/graph/graph.js
@@ -22,14 +22,15 @@ class Graph {
   }
 
   addValue(value){
-    this.addVertex(new Vertex(value));
+    this.addNode(new Vertex(value));
   }
 
-  addVertex(vertex){
-    this._adjacencyList.set(vertex, []);
+  addNode(vertex){
+    let newNode = this._adjacencyList.set(vertex, []);
+    return newNode;
   }
 
-  addDirectedEdge(startVertex, endVertex, weight = 0){
+  addEdge(startVertex, endVertex, weight = 0){
     if(!this._adjacencyList.has(startVertex) || !this._adjacencyList.has(endVertex)){
       throw new Error('ERROR: invalid vertices');
     }
@@ -97,22 +98,22 @@ const three = new Vertex(3);
 const oh = new Vertex(0);
 const nine = new Vertex(9);
 
-graph.addVertex(eight);
-graph.addVertex(six);
-graph.addVertex(seven);
-graph.addVertex(five);
-graph.addVertex(three);
-graph.addVertex(oh);
-graph.addVertex(nine);
+graph.addNode(eight);
+graph.addNode(six);
+graph.addNode(seven);
+graph.addNode(five);
+graph.addNode(three);
+graph.addNode(oh);
+graph.addNode(nine);
 
-graph.addDirectedEdge(eight, six);
-graph.addDirectedEdge(eight, five);
-graph.addDirectedEdge(six, seven);
-graph.addDirectedEdge(seven, five);
-graph.addDirectedEdge(five, three);
-graph.addDirectedEdge(three, oh);
-graph.addDirectedEdge(oh, nine);
-graph.addDirectedEdge(nine, eight);
+graph.addEdge(eight, six);
+graph.addEdge(eight, five);
+graph.addEdge(six, seven);
+graph.addEdge(seven, five);
+graph.addEdge(five, three);
+graph.addEdge(three, oh);
+graph.addEdge(oh, nine);
+graph.addEdge(nine, eight);
 
 console.log(graph);
 console.log(graph.getNeighbors(eight));
