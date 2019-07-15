e5dbcc1d984f33ad509ea45ea6ee2c03c6273628
JeffLawrence1 <jefflaw13@hotmail.com>
2019-07-15 13:46:54 -0700

    breadthfirst

--- code-challenges/401/breadthFirst/breadthFirst.js
+++ code-challenges/401/breadthFirst/breadthFirst.js
@@ -1,2 +1,26 @@
 'use strict';
 
+
+function breadthFirst(startNode){
+  let visitedArray = [];
+  for(let i = 0; i < this.numberOfNodes; i++){
+    visitedArray[i] = false;
+    let queue = [];
+    visitedArray[startNode] = true;
+    queue.push(startNode);
+    while(queue.length){
+      let deQ = queue.shift();
+      console.log(deQ);
+      let adList = this._adjacencyList.get(deQ);
+      for(let j in adList){
+        let neighbor = adList[j];
+        if(!visitedArray[neighbor]){
+          visitedArray[neighbor] = true;
+          queue.push(neighbor);
+        }
+      }
+    }
+  }
+}
+
+module.exports = breadthFirst;
